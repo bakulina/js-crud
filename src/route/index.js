a680a281db49e3d711f541e4f289d6811794e085
bakulina <bakulinaelena800@gmail.com>
2024-07-15 10:22:29 +0200

    servis

--- src/route/index.js
+++ src/route/index.js
@@ -2,112 +2,97 @@
 const express = require('express')
 // Cтворюємо роутер - місце, куди ми підключаємо ендпоїнти
 const router = express.Router()
-
-// ================================================================
 class User {
-  static #list = []
-  constructor(email, login, password) {
+  constructor(name, messenger, email) {
+    this.name = name
+    this.messenger = messenger
     this.email = email
-    this.login = login
-    this.password = password
-    this.id = new Date().getTime()
-  }
-  verifyPassword = (password) => this.password === password
-  static add = (user) => {
-    this.#list.push(user)
-  }
-  static getList = () => this.#list
-
-  static getById = (id) =>
-    this.#list.find((user) => user.id === id)
-
-  static deleteById = (id) => {
-    const index = this.#list.findIndex(
-      (user) => user.id === id,
-    )
-    if (index !== -1) {
-      this.#list.splice(index, 1)
-      return true
-    } else {
-      return false
-    }
-  }
-  static update = (user, { email }) => {
-    if (email) {
-      user.email = email
-    }
-  }
-  static updateById = (id, { email }) => {
-    const user = this.getById(id)
-    if (user) {
-      this.update(user, data)
-    }
   }
 }
+// ================================================================
+
+//======================================================================
 
 //=================================================================
 // router.get Створює нам один ентпоїнт
 
 // ↙️ тут вводимо шлях (PATH) до сторінки
-router.get('/', function (req, res) {
-  // res.render генерує нам HTML сторінку
-  const list = User.getList()
-
-  // ↙️ cюди вводимо назву файлу з сontainer
+router.get('/index', function (req, res) {
   res.render('index', {
     // вказуємо назву папки контейнера, в якій знаходяться наші стилі
     style: 'index',
-
-    data: {
-      users: {
-        list,
-        isEmpty: list.length === 0,
-      },
-    },
+    data: {},
   })
+
+  // res.render генерує нам HTML сторінку
   // ↑↑ сюди вводимо JSON дані
 })
 
 // ================================================================
 
-router.post('/user-create', function (req, res) {
-  const { email, login, password } = req.body
-  const user = new User(email, login, password)
-  User.add(user)
-  console.log(User.getList())
-  res.render('success-info', {
-    style: 'success-info',
-    info: 'Користувач створенний',
+router.get('/sluzby', function (req, res) {
+  const id = Number(req.query.id)
+  res.render('sluzby', {
+    style: 'sluzby',
   })
 })
+
 // ================================================================
 
-router.get('/user-delete', function (req, res) {
-  const { id } = req.query
-
-  User.deleteById(Number(id))
-
-  res.render('success-info', {
-    style: 'success-info',
-    info: 'Користувач видаленний',
+router.get('/plyn', function (req, res) {
+  const id = Number(req.query.id)
+  res.render('plyn', {
+    style: 'plyn',
   })
 })
+
 // ================================================================
 
-router.post('/user-update', function (req, res) {
-  const { email, password, id } = req.body
-  let result = false
-  const user = User.getById(Number(id))
+router.get('/Tepel', function (req, res) {
+  const id = Number(req.query.id)
+  res.render('Tepel', {
+    style: 'Tepel',
+  })
+})
 
-  if (verifyPassword(password)) {
-    User.update(user, { email })
-    result = true
-  }
-  console.log(email, password, id)
+//===============================================
+router.get('/klimat', function (req, res) {
+  const id = Number(req.query.id)
+  res.render('klimat', {
+    style: 'klimat',
+  })
+})
+//======================================================
+router.get('/voda', function (req, res) {
+  const id = Number(req.query.id)
 
-  res.render('success-info', {
-    style: 'success-info',
-    info: result ? 'пошта оновленна' : 'сталося помилка',
+  res.render('voda', {
+    style: 'voda',
+  })
+})
+//==============================================
+
+//===================================
+router.get('/prace', function (req, res) {
+  const id = Number(req.query.id)
+
+  res.render('prace', {
+    style: 'prace',
+  })
+})
+//=================================================
+router.get('/electro', function (req, res) {
+  const id = Number(req.query.id)
+
+  res.render('electro', {
+    style: 'electro',
+  })
+})
+router.get('/stavba', function (req, res) {
+  const id = Number(req.query.id)
+
+  res.render('stavba', {
+    style: 'stavba',
   })
 })
 
